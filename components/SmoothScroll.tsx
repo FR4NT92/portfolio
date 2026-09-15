7d8c2ba82e8be320783922d3872b059f17f4bbc0
Francisco Truppa <frantruppa@gmail.com>
2026-09-15 13:11:37 -0300

    fix: typecast completo de ReactLenis a any

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -2,11 +2,14 @@
 
 import { ReactLenis } from '@studio-freight/react-lenis';
 
+// 1. Desvinculamos los tipos estrictos de la librería renombrándola como "any"
+const Lenis = ReactLenis as any;
+
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   return (
-    <ReactLenis root>
-      {/* Al decirle "as any", obligamos a TypeScript a ignorar la versión */}
-      {children as any}
-    </ReactLenis>
+    // 2. Usamos nuestra versión libre de tipos
+    <Lenis root>
+      {children}
+    </Lenis>
   );
 }
