89a3a2c4c4cd00389e55d92f13b171787786ac7f
Francisco Truppa <frantruppa@gmail.com>
2026-09-15 13:30:36 -0300

    feat: instalar lenis oficial y recuperar smooth scroll

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -1,5 +1,28 @@
 "use client";
 
+import { useEffect } from 'react';
+import Lenis from 'lenis';
+
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    // Inicializamos Lenis de forma nativa
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    // Limpiamos la instancia al desmontar para evitar bugs de memoria
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return <>{children}</>;
 }
