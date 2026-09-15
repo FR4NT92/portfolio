e8b155c3b46c66d52d832c71234b52df0bd2738d
FR4NT92 <frantruppa@gmail.com>
2026-09-15 13:26:05 -0300

    Remove problematic library import from SmoothScroll
    Removed problematic library import and kept child components intact.

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-// Eliminamos la importación de la librería problemática. 
-// Solo renderizamos a los componentes hijos intactos.
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
