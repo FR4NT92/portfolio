1f3d82d2ac9c4955c70e025491c7242d9e969755
FR4NT92 <frantruppa@gmail.com>
2026-09-16 18:17:52 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,16 @@
 import Hero from "@/components/Hero";
 import ProjectsStack from "@/components/ProjectsStack";
 import Footer from "@/components/Footer";
-import SmoothScroll from "@/components/SmoothScroll";
 
 export default function Home() {
   return (
-    <SmoothScroll>
-      {/* EL FONDO GLOBAL: Gobierna toda la web y evita recuadros cortados */}
-      <main 
-        className="w-full min-h-screen bg-[#111] overflow-hidden bg-cover bg-center bg-fixed"
-        style={{ backgroundImage: "url('/bg.png')" }}
-      >
-        <Hero />
-        <ProjectsStack />
-        <Footer />
-      </main>
-    </SmoothScroll>
+    <main 
+      className="w-full min-h-screen bg-[#111] overflow-hidden bg-cover bg-center bg-fixed"
+      style={{ backgroundImage: "url('/bg.png')" }}
+    >
+      <Hero />
+      <ProjectsStack />
+      <Footer />
+    </main>
   );
 }
