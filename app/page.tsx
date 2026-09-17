1acd43ec3135f2a31582d07327fae8158610b02c
FR4NT92 <frantruppa@gmail.com>
2026-09-17 08:50:20 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main 
       // ELIMINADO el overflow-hidden que rompía el anclaje (sticky)
       className="w-full min-h-screen bg-[#111] bg-cover bg-center bg-fixed"
-      style={{ backgroundImage: "url('/bg.jpg')" }}
+      style={{ backgroundImage: "url('/bg.png')" }}
     >
       <Hero />
       <ProjectsStack />
